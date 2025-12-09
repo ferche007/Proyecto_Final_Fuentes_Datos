77c6c5c052a0dca97dc3e3223a9bcb76cbc43d7e
leovice2004 <160078217+leovice2004@users.noreply.github.com>
2025-12-09 00:43:42 -0600

    Edited Main.py

--- resources/main.py
+++ resources/main.py
@@ -9,6 +9,7 @@ import math
 datos = None
 
 
+##Calculo MAS
 def calcula_mas():
     calculo_mas = tk.Tk()
     calculo_mas.title("Cálculo por MAS")
@@ -38,8 +39,6 @@ def calcula_mas():
     )
     err_est_total = math.sqrt(est_var_total)
     err_est_prom = math.sqrt(est_var_prom)
-    inter_conf_total = (1.96) * (err_est_total)
-    inter_conf_prom = (1.96) * (err_est_prom)
 
     ##---------Muestra-------------#
     df_muestra = datos_mas[datos_mas["Id"].isin(unidades_muestra)]
@@ -87,6 +86,208 @@ def calcula_mas():
     calculo_mas.mainloop()
 
 
+##Calculo ER
+def calcula_er():
+    calculo_er = tk.Tk()
+    calculo_er.title("Cálculo por Estimador Razón")
+    calculo_er.geometry("1000x900")
+    datos_er = datos
+    col_var = entrada_var.get()
+    col_var_aux = entrada_var_aux.get()
+    tamanio_muestra = int(entrada_tamanio.get())
+    longitud_columna = len(datos_er[col_var])
+    er.destroy()
+    unidades_muestra = random.sample(range(1, longitud_columna + 1), tamanio_muestra)
+    valores_prim = np.array([datos_er.iloc[i - 1][col_var] for i in unidades_muestra])
+    valores_aux = np.array(
+        [datos_er.iloc[j - 1][col_var_aux] for j in unidades_muestra]
+    )
+    valor_cruz = valores_prim * valores_aux
+    sum_valor_cruz = np.sum(valor_cruz)
+    sum_primarios = np.sum(valores_prim)
+    sum_aux = np.sum(valores_aux)
+    cuadrados = valores_prim**2
+    cuadrados_aux = valores_aux**2
+    sum_cua_prim = np.sum(cuadrados)
+    sum_cua_aux = np.sum(cuadrados_aux)
+    aux_total = datos_er[col_var_aux].sum()
+    aux_prom = aux_total / longitud_columna
+    r_gorr = sum_primarios / sum_aux
+    est_total = (aux_total) * (sum_primarios / sum_aux)
+    est_promedio = (aux_prom) * (sum_primarios / sum_aux)
+    sr_chic = (1 / (tamanio_muestra - 1)) * (
+        sum_cua_prim - (2 * r_gorr * (sum_valor_cruz)) + ((r_gorr**2) * sum_cua_aux)
+    )
+    var_total = (
+        (longitud_columna**2)
+        * (1 - (tamanio_muestra / longitud_columna))
+        * (sr_chic / tamanio_muestra)
+    )
+    var_prom = (1 - (tamanio_muestra / longitud_columna)) * (sr_chic / tamanio_muestra)
+    err_est_total = math.sqrt(var_total)
+    err_est_prom = math.sqrt(var_prom)
+    ##---------Muestra-------------#
+    df_muestra = datos_er[datos_er["Id"].isin(unidades_muestra)]
+    marco_df_muestra = ttk.LabelFrame(calculo_er, text="Datos Muestra", padding=10)
+    marco_df_muestra.pack(fill="both", expand=True, padx=10, pady=10)
+    tabla_muestra = ttk.Treeview(
+        marco_df_muestra, columns=list(df_muestra.columns), show="headings", height=5
+    )
+    tabla_muestra.pack(fill="both", expand=True)
+    for col in df_muestra.columns:
+        tabla_muestra.heading(col, text=col)
+        tabla_muestra.column(col, width=120)
+    for _, fila in df_muestra.iterrows():
+        tabla_muestra.insert("", "end", values=list(fila))
+    scroll_y = ttk.Scrollbar(
+        marco_df_muestra, orient="vertical", command=tabla_muestra.yview
+    )
+    tabla_muestra.configure(yscrollcommand=scroll_y.set)
+    scroll_y.pack(side="right", fill="y")
+    ##-----------------
+    ##-------Resultados
+    marco_res = ttk.LabelFrame(calculo_er, text="Resultados", padding=10)
+    marco_res.pack(fill="x", padx=10, pady=10)
+    ttk.Label(marco_res, text=f"Estimador Total: {est_total}").pack(anchor="w", pady=3)
+    ttk.Label(marco_res, text=f"Estimador Promedio: {est_promedio}").pack(
+        anchor="w", pady=3
+    )
+    ttk.Label(
+        marco_res,
+        text=f"Estimador de Varianza del estimador del total: {var_total}",
+    ).pack(anchor="w", pady=3)
+    ttk.Label(
+        marco_res,
+        text=f"Estimador de Varianza del estimador del promedio: {var_prom}",
+    ).pack(anchor="w", pady=3)
+    ttk.Label(marco_res, text=f"Estimador Error est Total: {err_est_total}").pack(
+        anchor="w", pady=3
+    )
+    ttk.Label(marco_res, text=f"Estimador Error est promedio: {err_est_prom}").pack(
+        anchor="w", pady=3
+    )
+    ##------------------
+    calculo_er.mainloop()
+
+
+def calcula_est():
+    calculo_est = tk.Tk()
+    calculo_est.title("Cálculo por Estimador Razón")
+    calculo_est.geometry("1000x900")
+    datos_est = datos
+    col_var = entrada_var.get()
+    longitud_columna = len(datos_est[col_var])
+    tamanio_muestra = int(entrada_tamanio.get())
+    col_estratos = entrada_est.get()
+    estratos = estratos = [
+        x.strip() for x in entrada_estratos_cu.get().strip("()").split(",")
+    ]
+    est.destroy()
+    num_cada_est = []
+    df_estratos = []
+    for i in estratos:
+        numero = (datos_est[col_estratos].astype(str) == i).sum()
+        num_cada_est.append(numero)
+        df_estratos.append(datos_est[datos_est[col_estratos].astype(str) == i])
+    n_prop_est = []
+    for j in num_cada_est:
+        n_prop = tamanio_muestra * (j / longitud_columna)
+        n_prop_est.append(n_prop)
+    muestras_estratos = []
+    ids_por_estrato = []
+
+    for idx, df_est_i in enumerate(df_estratos):
+        n = int(n_prop_est[idx])
+        if n > 0:
+            muestra_i = df_est_i.sample(n=n, random_state=1)
+        else:
+            muestra_i = df_est_i.head(0)
+
+        muestras_estratos.append(muestra_i)
+
+        ids_estrato_i = muestra_i["Id"].tolist()
+        ids_por_estrato.append(ids_estrato_i)
+    valores_estrato = []
+    for i in ids_por_estrato:
+        valores_estrato.append(np.array([datos_est.iloc[x - 1][col_var] for x in i]))
+    suma = []
+    for i in valores_estrato:
+        suma.append(np.sum(i))
+    cuadrados = []
+    for i in valores_estrato:
+        cuadrados.append(i**2)
+    suma_cuadrados = []
+    for i in cuadrados:
+        suma_cuadrados.append(np.sum(i))
+    est_total = []
+    for i, j, z in zip(suma, num_cada_est, n_prop_est):
+        est_total.append((i / z) * j)
+    est_promedio = []
+    for i, j, z in zip(suma, n_prop_est, num_cada_est):
+        est_promedio.append((i / j) * (z / longitud_columna))
+    est_total_bueno = np.sum(est_total)
+    est_promedio_bueno = np.sum(est_promedio)
+    var_tot = []
+    for Nh, nh, valores in zip(num_cada_est, n_prop_est, valores_estrato):
+
+        nh = int(nh)
+
+        if nh > 1:
+            Sh2 = np.var(valores, ddof=1)
+            f = nh / Nh
+            var_h = (Nh**2) * (1 - f) * (Sh2 / nh)
+        else:
+            var_h = 0
+
+        var_tot.append(var_h)
+
+    var_total_estimador = np.sum(var_tot)
+
+    var_prom = []
+    for Nh, nh, valores in zip(num_cada_est, n_prop_est, valores_estrato):
+
+        nh = int(nh)
+
+        if nh > 1:
+            Sh2 = np.var(valores, ddof=1)
+            f = nh / Nh
+            Wh = Nh / longitud_columna
+            var_h = (Wh**2) * (1 - f) * (Sh2 / nh)
+        else:
+            var_h = 0
+
+        var_prom.append(var_h)
+    var_promedio_estimador = np.sum(var_prom)
+    err_est_prom = math.sqrt(var_promedio_estimador)
+    err_est_total = math.sqrt(var_total_estimador)
+
+    ##-------Resultados
+    marco_res = ttk.LabelFrame(calculo_est, text="Resultados", padding=10)
+    marco_res.pack(fill="x", padx=10, pady=10)
+    ttk.Label(marco_res, text=f"Estimador Total: {est_total_bueno}").pack(
+        anchor="w", pady=3
+    )
+    ttk.Label(marco_res, text=f"Estimador Promedio: {est_promedio_bueno}").pack(
+        anchor="w", pady=3
+    )
+    ttk.Label(
+        marco_res,
+        text=f"Estimador de Varianza del estimador del total: {var_total_estimador}",
+    ).pack(anchor="w", pady=3)
+    ttk.Label(
+        marco_res,
+        text=f"Estimador de Varianza del estimador del promedio: {var_promedio_estimador}",
+    ).pack(anchor="w", pady=3)
+    ttk.Label(marco_res, text=f"Estimador Error est Total: {err_est_total}").pack(
+        anchor="w", pady=3
+    )
+    ttk.Label(marco_res, text=f"Estimador Error est promedio: {err_est_prom}").pack(
+        anchor="w", pady=3
+    )
+    ##------------------
+    calculo_est.mainloop()
+
+
 ##Ventana MAS
 def est_MAS():
     global entrada_var, entrada_tamanio, mas
@@ -130,12 +331,14 @@ def est_ER():
     label_tamanio_muestra.place(x=20, y=80)
     entrada_tamanio = tk.Entry()
     entrada_tamanio.place(x=320, y=80)
+    boton_calcular = tk.Button(er, text="Calcular", command=calcula_er)
+    boton_calcular.place(x=20, y=100)
     er.mainloop()
 
 
 ##Ventana EST
 def est_EST():
-    global entrada_var, entrada_tamanio, entrada_est, est
+    global entrada_var, entrada_tamanio, entrada_est, est, entrada_estratos_cu
     segunda.destroy()
     est = tk.Tk()
     est.title("Estimación por Estratificación")
@@ -154,6 +357,15 @@ def est_EST():
     label_tamanio_muestra.place(x=20, y=80)
     entrada_tamanio = tk.Entry()
     entrada_tamanio.place(x=320, y=80)
+    label_estratos_cu = tk.Label(
+        est,
+        text="¿Qué estratos hay? EJ. Si estratos estan en tabla como H hombre y M mujer (H,M)",
+    )
+    label_estratos_cu.place(x=20, y=100)
+    entrada_estratos_cu = tk.Entry()
+    entrada_estratos_cu.place(x=560, y=100)
+    boton_calcular = tk.Button(est, text="Calcular", command=calcula_est)
+    boton_calcular.place(x=20, y=140)
     est.mainloop()
 
 
